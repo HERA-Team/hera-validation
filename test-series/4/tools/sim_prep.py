3d66d9f88b36783dc14913ea00530a0a93512167
Bobby Pascua <r.pascua@berkeley.edu>
2020-03-26 23:58:37 -0600

    One big commit (oops). Updated add_noise function to automatically find key for autocorrelation (so noise can be added pre-inflation) and allow the user to specify a receiver temperature. Added some comments with concerns/questions. Fleshed out most of the placeholder functions in sim_prep; only remaining step for generating H1C-like files from RIMEz files and reference H1C files is to develop the code for finding the intersection between the arrays and appropriately downselecting the RIMEz data. Also a bunch of small formatting changes to docstrings and comments.

--- test-series/4/tools/sim_prep.py
+++ test-series/4/tools/sim_prep.py
@@ -1,4 +1,4 @@
-import pathlib
+import re
 import numpy as np
 from scipy import interpolate
 
@@ -13,7 +13,7 @@ else:
     from hera_sim.utils import _listify
 
 
-def add_noise(sim, seed=1010):
+def add_noise(sim, Trx=100, seed=1010):
     """
     Add thermal noise to a UVData object, based on its auto-correlations.
 
@@ -21,6 +21,8 @@ def add_noise(sim, seed=1010):
     ----------
     sim : :class:`pyuvdata.UVData` or :class:`hera_sim.Simulator` instance
         The simulation data to add noise to
+    Trx : float, optional
+        The receiver temperature, in Kelvin.
     seed : int, optional
         The random seed.
     """
@@ -30,15 +32,29 @@ def add_noise(sim, seed=1010):
     lsts = np.unique(sim.data.lst_array)
     freqs = np.unique(sim.data.freq_array)
 
+    # Find out which antenna has the autocorrelation data, since we'll
+    # add noise before inflating and adding other systematics.
+    antpair = list(
+        [ants for ants in sim.data.get_antpairs() if ants[0] == ants[1]]
+    )[0]
+
     # Set up to use the autos to set the noise level
     freqs_GHz = freqs / 1e9  # GHz
+    # XXX Has anyone checked that the h1c beam polynomial used in hera_sim
+    # actually matches the beam used in the RIMEz simulation?
     omega_p = noise.bm_poly_to_omega_p(freqs_GHz)
     Jy_to_K = noise.jy2T(freqs_GHz, omega_p) / 1000
-    autos = sim.data.get_data((0, 0, 'xx')) * Jy_to_K[None, :]
+    
+    # XXX The xx and yy polarizations are rotated 90 degrees relative to one
+    # another, so does it make sense to use the xx polarization for adding 
+    # noise to the data for both the xx and yy polarizations?
+    autos = sim.data.get_data(*antpair, 'xx') * Jy_to_K[None, :]
     autos_interp = interpolate.RectBivariateSpline(lsts, freqs_GHz, autos.real)
 
     np.random.seed(seed)
-    noise = sim.add_noise('thermal_noise', Tsky_mdl=autos_interp, ret_vis=True)
+    noise = sim.add_noise(
+        'thermal_noise', Tsky_mdl=autos_interp, Trx=Trx, ret_vis=True
+    )
     return sim, noise
 
 def adjust_sim_to_data(sim_file, data_files, save_dir, sky_cmp=None, clobber=True):
@@ -47,13 +63,13 @@ def adjust_sim_to_data(sim_file, data_files, save_dir, sky_cmp=None, clobber=Tru
     
     Parameters
     ----------
-    sim_file : str or :class:`pathlib.Path`
+    sim_file : str or path-like object
         Path to simulation file to modify.
         
-    data_files : array-like of str or :class:`pathlib.Path`
+    data_files : array-like of str or path-like objects
         Collection of paths to observed data.
         
-    save_dir : str or :class:`pathlib.Path`
+    save_dir : str or path-like object
         Path to directory where modified simulation files should be saved.
     
     sky_cmp : str, optional
@@ -75,33 +91,33 @@ def adjust_sim_to_data(sim_file, data_files, save_dir, sky_cmp=None, clobber=Tru
     baseline_map : dict
         Dictionary mapping unique reference baselines to unique simulation baselines.
     """
-    # make sure the data files are a list and load in their metadata
+    # Ensure the data files are a list and load in their metadata.
     data_files = _listify(data_files)
     ref_uvd = UVData()
-    ref_uvd.read_uvh5(data_files, read_data=False)
+    ref_uvd.read(data_files, read_data=False)
     
-    # get the sky component if not specified
+    # Get the sky component if not specified
     sky_cmp = sky_cmp or _parse_filename_for_cmp(sim_file)
     
-    # downselect in polarization
+    # Load in the simulation data, but only the linear vispols.
     use_pols = [polstr2num(pol) for pol in ('xx', 'yy')]
     sim_uvd = UVData()
-    sim_uvd.read_uvh5(sim_file, polarizations=use_pols)
+    sim_uvd.read(sim_file, polarizations=use_pols)
     
-    # modify the antenna labels and data array for the simulation
-    sim_uvd, ref_ants, sim_ants, baseline_map = downselect_antennas(sim_uvd, ref_uvd)
+    # Find and use the intersection of the RIMEz and H1C arrays.
+    ref_ants, sim_ants, baseline_map = downselect_antennas(sim_uvd, ref_uvd)
     
-    # downselect in time and rephase lsts
-    sim_uvd = rephase_to_reference(sim_uvd, ref_uvd)
+    # Downselect in time and rephase LSTs to match the reference data.
+    rephase_to_reference(sim_uvd, ref_uvd)
     
-    # chunk the simulation data and save to disk
+    # Chunk the simulation data and write to disk.
     chunk_sim_and_save(sim_uvd, data_files[0], save_dir, sky_cmp, clobber)
     
     return ref_ants, sim_ants, baseline_map
 
-def downselect_antennas(sim_uvd, ref_uvd):
+def downselect_antennas(sim_uvd, ref_uvd, inplace=True):
     """
-    Downselect simulated array antennas to maximally match unique baselines between data sets.
+    Downselect simulated array to match unique baselines from reference.
     
     Parameters
     ----------
@@ -109,16 +125,17 @@ def downselect_antennas(sim_uvd, ref_uvd):
         :class:`pyuvdata.UVData` object containing the simulation data.
         
     ref_uvd : :class:`pyuvdata.UVData`
-        :class:`pyuvdata.UVData` object containing the reference data. Only needs 
-        to have the metadata loaded.
+        :class:`pyuvdata.UVData` object containing the reference data. 
+        Only needs to have the metadata loaded.
                 
     Returns
     -------
     new_sim_uvd : :class:`pyuvdata.UVData`
-        :class:`pyuvdata.UVData` object whose antennas have been relabeled, and whose 
-        data array has been rearranged to reflect the change in antenna labels. If 
-        the number of antennas to keep is less than those in the simulated data, then 
-        a downselection is performed on the simulated data.
+        :class:`pyuvdata.UVData` object whose antennas have been relabeled, 
+        and whose data array has been rearranged to reflect the change in 
+        antenna labels. If the number of antennas to keep is less than 
+        those in the simulated data, then a downselection is performed on 
+        the simulated data.
         
     ref_ants : list of int
         List of antenna numbers that remain in the reference data.
@@ -127,19 +144,24 @@ def downselect_antennas(sim_uvd, ref_uvd):
         List of antenna numbers that remain in the simulated data.
         
     baseline_map : dict
-        Dictionary mapping unique baselines between data sets. The keys of this 
-        dictionary are the antenna pairs from the reference data, and the values 
-        are the antenna pairs from the simulated data.
+        Dictionary mapping unique baselines between data sets. The keys 
+        of this dictionary are the antenna pairs from the reference data, 
+        and the values are the antenna pairs from the simulated data.
     """
     # TODO: actually write the code
-    return sim_uvd, None, None, None
+    ref_ants, sim_ants, baseline_map = [], [], {}
 
-def rephase_to_reference(sim_uvd, ref_uvd):
+    if not inplace:
+        return ref_ants, sim_ants, baseline_map
+
+    return sim_uvd, ref_ants, sim_ants, baseline_map
+
+def rephase_to_reference(sim_uvd, ref_uvd, inplace=True):
     """
     Rephase simulation LSTs to reference LSTs after downselection in time.
     
-    After downselection and rephasing, this function overwrites the simulation LST 
-    and time arrays to match the reference LSTs and times.
+    After downselection and rephasing, this function overwrites the 
+    simulation LST and time arrays to match the reference LSTs and times.
     
     Parameters
     ----------
@@ -147,19 +169,71 @@ def rephase_to_reference(sim_uvd, ref_uvd):
         :class:`pyuvdata.UVData` object containing the simulation data.
         
     ref_uvd : :class:`pyuvdata.UVData`
-        :class:`pyuvdata.UVData` object containing the reference data. Only the 
-        metadata for this object needs to be read.
+        :class:`pyuvdata.UVData` object containing the reference data. 
+        Only the metadata for this object needs to be read.
         
+    inplace : bool, optional
+        Whether to perform the rephasing and downselection on the 
+        simulated data or on a copy of the data. Default is to perform 
+        the downselection and rephasing in-place.
+
     Returns
     -------
-    new_sim_uvd : :class:`pyuvdata.UVData`
-        :class:`pyuvdata.UVData` object containing the modified simulation data. The 
-        modified data has been rephased to match the reference LSTs and has had its 
-        time and LST arrays adjusted to contain only the times and LSTs that are 
-        present in the reference data.
+    new_sim_uvd : :class:`hera_cal.io.HERAData`
+        :class:`hera_cal.io.HERAData` object containing the modified 
+        simulation data. The modified data has been rephased to match 
+        the reference LSTs and has had its time and LST arrays adjusted 
+        to contain only the times and LSTs that are present in the 
+        reference data. Only returned if ``inplace`` is set to False.
     """
-    # TODO: actually write the code
-    return sim_uvd
+    # Convert the simulation to a HERAData object. 
+    use_uvd = sim_uvd if inplace else copy.deepcopy(sim_uvd)
+    hd = to_HERAData(use_uvd)
+    hd_metas = hd.get_metadata_dict()
+
+    # Load in useful metadata.
+    sim_lsts = hd_metas['lsts']
+    sim_times = hd_metas['times']
+    ref_lsts = np.unique(ref_uvd.lst_array)
+    ref_times = np.unique(ref_uvd.time_array)
+    
+    # Find out which times to use and how much to rephase in LST
+    start_lst = ref_lsts[0]
+    dist_from_start = np.abs(sim_lsts - start_lst)
+    start_ind = np.argwhere(
+        dist_from_start == dist_from_start.min()
+    ).flatten()[0]
+    lst_slice = slice(start_ind, start_ind + ref_uvd.Ntimes)
+    use_times = sim_times[lst_slice]
+    use_lsts = sim_lsts[lst_slice]
+    dlst = ref_lsts - use_lsts
+
+    # Downselect in time and load data.
+    hd.select(times=use_times, keep_all_metadata=False)
+    data, _, _ = hd.build_datacontainers()
+
+    # Build the baseline -> ENU baseline vector dictionary.
+    antpos, ants = hd.get_ENU_antpos()
+    bls = {bl : None for bl in data.bls()}
+    ants = list(ants)
+    for bl in bls:
+        ai, aj = bl[:2]
+        i, j = ants.index(ai), ants.index(aj)
+        bls[bl] = antpos[i] - antpos[j]
+
+    # Rephase and update the data.
+    hera_cal.utils.lst_rephase(data, bls, hd_metas['freqs'], dlst)
+    hd.update(data=data)
+    new_sim_lsts = np.zeros_like(hd.lst_array)
+    new_sim_times = np.zeros_like(hd.time_array)
+    for use_time, ref_time, ref_lst in zip(use_times, ref_times, ref_lsts):
+        blt_slice = np.argwhere(sim_times == use_time)
+        new_sim_lsts[blt_slice] = ref_lst
+        new_sim_times[blt_slice] = ref_time
+    hd.time_array = new_sim_times
+    hd.lst_array = new_sim_lsts
+
+    return hd if not inplace else None
 
 def chunk_sim_and_save(sim_uvd, ref_file, save_dir, sky_cmp, clobber=True):
     """
@@ -168,27 +242,50 @@ def chunk_sim_and_save(sim_uvd, ref_file, save_dir, sky_cmp, clobber=True):
     Parameters
     ----------
     sim_uvd : :class:`pyuvdata.UVData`
-        :class:`pyuvdata.UVData` object containing the simulation data to chunk 
-        and write to disk.
+        :class:`pyuvdata.UVData` object containing the simulation data 
+        to chunk and write to disk.
         
     ref_file : str
-        Path to a file to use for reference when chunking. It is assumed that all 
-        files for the same JD have the same number of integrations as the reference 
-        file.
+        Path to a file to use for reference when chunking. It is assumed 
+        that all files for the same JD have the same number of 
+        integrations as the reference file.
         
-    save_dir : str or :class:`pathlib.Path`
+    save_dir : str or path-like object
         Path to the directory where the chunked files will be saved.
         
     sky_cmp : str
-        String denoting which sky component has been simulated. Should be one of 
-        the following: ('foregrounds', 'eor', 'sum').
+        String denoting which sky component has been simulated. Should 
+        be one of the following: ('foregrounds', 'eor', 'sum').
         
     clobber : bool, optional
-        Whether to overwrite any existing files that share the new filenames. Default 
-        is to overwrite files.
+        Whether to overwrite any existing files that share the new 
+        filenames. Default is to overwrite files.
     """
-    # TODO: actually write the code
-    pass
+    # get some useful metadata
+    uvd = UVData()
+    uvd.read(ref_file, read_data=False)
+    integrations_per_file = uvd.Ntimes
+    times = np.unique(sim_uvd.time_array)
+    jd_major = int(np.floor(times[0]))
+    Nfiles = int(times.size / integrations_per_file)
+    
+    # actually chunk and save the data
+    for Nfile in range(Nfiles):
+        start = Nfile * integrations_per_file
+        stop = start + integrations_per_file
+        use_times = times[start:stop]
+        jd_minor = str(times[start] - jd_major)[2:7]
+        filename = f"zen.{jd_major}.{jd_minor}.{sky_cmp}.uvh5"
+        save_path = os.path.join(save_dir, filename)
+        if os.path.exists(save_path):
+            if not clobber:
+                print(f"File {save_path} exists; skipping.")
+                continue
+            if clobber:
+                print(f"File {save_path} exists; clobbering.")
+        this_uvd = sim_uvd.select(times=use_times, inplace=False)
+        this_uvd.write_uvh5(save_path)
+    return
 
 def _parse_filename_for_cmp(filename):
     """
@@ -196,12 +293,15 @@ def _parse_filename_for_cmp(filename):
     
     Parameters
     ----------
-    filename : str or :class:`pathlib.Path`
+    filename : str or path-like object
         Name of the file to parse; may be a path to the file.
         
     Returns
     -------
     sky_cmp : str
-        Inferred sky component. Should be one of the following: ('foregrounds', 'eor', 'sum').
+        Inferred sky component. Should be one of the following: 
+        ('foregrounds', 'eor', 'sum').
     """
-    return None
+    cmp_re = re.compile("[a-z]+.uvh5")
+    sky_cmp = cmp_re.findall(str(filename))
+    return '' if not sky_cmp else sky_cmp[0][:-5]
